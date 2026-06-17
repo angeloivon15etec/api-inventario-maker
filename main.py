1e5bbe2dffc2b21961e2cdc43baa2d69681feb72
angeloivon15etec <angelo.ivon05@gmail.com>
2026-06-17 17:25:30 -0300

    feat: adiciona campo estado_conservacao e ajusta rota put

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from pydantic import BaseModel, Field
 # 1. Instanciação da API
 app = FastAPI(title="API Inova Lab - Inventário Maker")
 
-# 2. O Modelo Pydantic (A "Catraca" de Validação)
+# 2. O Modelo Pydantic (Atualizado com o campo obrigatório)
 class ComponenteSchema(BaseModel):
     nome: str = Field(..., min_length=2, description="Nome do componente maker")
     quantidade: int = Field(..., ge=0, description="Quantidade em estoque (deve ser maior ou igual a zero)")
     categoria: str = Field(..., description="Categoria do item (ex: Atuadores, Microcontroladores)")
+    estado_conservacao: str = Field(..., description="Estado de conservação do componente") # <-- FALTAVA ESTE CAMPO [cite: 6, 7]
 
 # 3. Nosso "Banco de Dados" temporário em memória
 estoque_laboratorio = [
-    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão"},
-    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores"},
-    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica"}
+    {"id": 1, "nome": "Arduino Sensor Shield", "quantidade": 15, "categoria": "Placas de Expansão", "estado_conservacao": "Novo"},
+    {"id": 2, "nome": "Micro Servo Motor SG90", "quantidade": 42, "categoria": "Atuadores", "estado_conservacao": "Usado"},
+    {"id": 3, "nome": "Esteira em Acrílico", "quantidade": 2, "categoria": "Mecânica", "estado_conservacao": "Excelente"}
 ]
 
 # Rota Raiz
@@ -30,13 +31,12 @@ def listar_componentes():
 # CRUD - CREATE (Cadastrar novo item)
 @app.post("/componentes", status_code=201)
 def adicionar_componente(novo_componente: ComponenteSchema):
-    # Lógica de autoincremento para o ID em memória
     if estoque_laboratorio:
         maior_id = max(item["id"] for item in estoque_laboratorio)
         novo_id = maior_id + 1
     else:
         novo_id = 1
-    # Converte o objeto Pydantic para dicionário e insere o ID
+        
     componente_dict = novo_componente.model_dump()
     componente_dict["id"] = novo_id
     
@@ -51,6 +51,7 @@ def atualizar_componente(componente_id: int, dados_atualizados: ComponenteSchema
             item["nome"] = dados_atualizados.nome
             item["quantidade"] = dados_atualizados.quantidade
             item["categoria"] = dados_atualizados.categoria
+            item["estado_conservacao"] = dados_atualizados.estado_conservacao # <-- ADICIONADO PARA ATUALIZAR O CAMPO 
             return {"mensagem": "Componente atualizado com sucesso!", "componente": item}
             
     raise HTTPException(status_code=404, detail="Componente não encontrado no laboratório.")
